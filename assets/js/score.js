fcfd6edc046c3a233dd5513f294525a4b98e7f9c
Monique <monique.meas@gmail.com>
2022-07-03 16:20:24 -0400

    modify file's name

--- assets/js/score.js
+++ assets/js/score.js
@@ -2,6 +2,7 @@ var username = document.getElementById('username');
 var saveScore = document.getElementById('saveScore');
 var finalScore = document.getElementById('finalScore');
 var mostRecentScore = localStorage.getItem('mostRecentScore');
+var highScore = JSON.parse(localStorage.getItem('highScore'));
 
 finalScore.innerText = mostRecentScore;
 username.addEventListener('keyup', () => {
--- assets/js/script.js
+++ assets/js/script.js
@@ -61,7 +61,7 @@ getNewQuestion = () => {
         localStorage.setItem("mostRecentScore", score);
 
         // got to the end of page
-        return window.location.assign ("/score.html");
+        return window.location.assign ("/end.html");
     }
     questionCounter++;
     questionCounterText.innerText = questionCounter + "/" + NumberQuestion;
